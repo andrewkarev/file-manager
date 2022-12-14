ff564d29694d190e0b4d444343c0998468772e9b
Andrew <an.carev4@gmail.com>
2022-12-14 13:26:34 +0300

    feat: implement a function to navigate to the top level directory

---
+++ src/navigation/goUp.js
@@ -0,0 +1,18 @@
+import { homedir } from 'node:os';
+import { chdir, cwd } from 'node:process';
+import { dirname } from 'node:path';
+import { printCurrentDir } from '../messages/printCurrentDir.js';
+
+export const goUp = () => {
+  const currentDir = cwd();
+  const dirToGo = dirname(currentDir);
+  const homeDir = homedir();
+
+  if (currentDir !== homeDir && dirToGo !== dirname(homeDir)) {
+    chdir(dirToGo);
+  }
+
+  printCurrentDir();
+
+  return;
+};
