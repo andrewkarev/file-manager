f81c7be44f0ceca41478ba3804ed7754df76ec46
Andrew <an.carev4@gmail.com>
2022-12-14 19:23:19 +0300

    feat: implement function to compress the file

---
+++ src/operations-with-file/compress.js
@@ -0,0 +1,72 @@
+import { join, dirname, basename } from 'node:path';
+import { cwd } from 'node:process';
+import { createReadStream, createWriteStream } from 'node:fs';
+import { createBrotliCompress } from 'node:zlib';
+import { CustomError } from '../errors/CustomError.js';
+import { OperationFailedError } from '../errors/OperationFailedError.js';
+import { checkFile } from '../utils/checkFile.js';
+import { checkIsFileExist } from '../utils/checkIsFileExist.js';
+import { printCurrentDir } from '../messages/printCurrentDir.js';
+import { validateFilename } from '../utils/validateFilename.js';
+
+export const compress = async ([pathToFile, pathToDestination]) => {
+  try {
+    if (!pathToFile) {
+      throw new CustomError('Please, provide a path to the source file');
+    }
+
+    if (!pathToDestination) {
+      throw new CustomError(
+        'Please, provide the path to save the compressed file'
+      );
+    }
+
+    const sourceFilePath = join(cwd(), pathToFile);
+    const compressedDirPath = join(cwd(), dirname(pathToDestination));
+    const isSourceFile = await checkIsFileExist(sourceFilePath);
+    const isDestinationDir = await checkIsFileExist(compressedDirPath);
+
+    if (!isSourceFile || !isDestinationDir) {
+      throw new CustomError(
+        'There is no file or directory with the specified name'
+      );
+    }
+
+    const filename = basename(sourceFilePath);
+    const isFile = await checkFile(sourceFilePath, filename);
+
+    if (!isFile) {
+      throw new CustomError('Only files can be compressed');
+    }
+
+    const compressedFileName = basename(pathToDestination).concat('.br');
+
+    if (await checkIsFileExist(compressedFileName)) {
+      throw new CustomError('A file with the same name already exists');
+    }
+
+    if (!validateFilename(compressedFileName)) {
+      throw new CustomError(
+        '"/,|,\\" and white spaces are not allowed in a filename'
+      );
+    }
+
+    const readStream = createReadStream(sourceFilePath);
+    const writeStream = createWriteStream(
+      join(compressedDirPath, compressedFileName)
+    );
+
+    const brotli = createBrotliCompress();
+
+    const stream = readStream.pipe(brotli).pipe(writeStream);
+
+    stream.on('finish', () => {
+      console.log('Done compressing');
+      printCurrentDir();
+    });
+  } catch (error) {
+    if (error instanceof CustomError) console.log(error.message);
+
+    throw new OperationFailedError('Operation failed');
+  }
+};
