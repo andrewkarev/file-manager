1e50f76cd63ad8bf49990b68e210fe6506486354
Andrew <an.carev4@gmail.com>
2022-12-16 13:07:13 +0300

    refactor: update working with absolute paths in the `move.js`

--- src/operations-with-file/move.js
+++ src/operations-with-file/move.js
@@ -1,12 +1,11 @@
 import { rm } from 'node:fs/promises';
-import { join } from 'node:path';
-import { cwd } from 'node:process';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
 import { copy } from './copy.js';
 
 export const move = async ([pathToOldFile, pathToNewDir]) => {
   await copy([pathToOldFile, pathToNewDir]);
 
-  const pathTofFile = join(cwd(), pathToOldFile);
+  const pathToFileToDelete = getAbsolutePath(pathToOldFile);
 
-  await rm(pathTofFile);
+  await rm(pathToFileToDelete);
 };
