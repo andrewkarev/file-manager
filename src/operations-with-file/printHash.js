bc9c07310cf1b6542f829ca2c3c5d0b973198355
Andrew <an.carev4@gmail.com>
2022-12-16 13:57:13 +0300

    refactor: add throwing `Invalid input` error on file hashing

--- src/operations-with-file/printHash.js
+++ src/operations-with-file/printHash.js
@@ -1,31 +1,32 @@
 import { createHash } from 'node:crypto';
 import { createReadStream } from 'fs';
 import { stdout } from 'process';
-import { join, basename } from 'node:path';
-import { cwd } from 'node:process';
 import { CustomError } from '../errors/CustomError.js';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
 import { checkIsFileExist } from '../utils/checkIsFileExist.js';
-import { checkFile } from '../utils/checkFile.js';
 import { printCurrentDir } from '../messages/printCurrentDir.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
+import { isFile } from '../utils/isFile.js';
 
 export const printHash = async ([path]) => {
   try {
     if (!path) {
-      throw new CustomError('Please, provide a path to the file for hashing');
+      throw new InvalidInputError(
+        'Please, provide a path to the file for hashing'
+      );
     }
 
-    const pathToFile = join(cwd(), path);
+    const pathToFile = getAbsolutePath(path);
     const isPathValid = await checkIsFileExist(pathToFile);
 
     if (!isPathValid) {
       throw new CustomError('There is no file with the specified name');
     }
 
-    const filename = basename(pathToFile);
-    const isFile = await checkFile(pathToFile, filename);
+    const isPathToFile = await isFile(pathToFile);
 
-    if (!isFile) {
+    if (!isPathToFile) {
       throw new CustomError('Only files can be hashed');
     }
 
@@ -39,6 +40,11 @@ export const printHash = async ([path]) => {
       printCurrentDir();
     });
   } catch (error) {
+    if (error instanceof InvalidInputError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
+    }
+
     if (error instanceof CustomError) console.log(error.message);
 
     throw new OperationFailedError('Operation failed');
