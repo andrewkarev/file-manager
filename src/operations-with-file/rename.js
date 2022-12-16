d600f55d232594f20a71ee77e7a755b7fe490984
Andrew <an.carev4@gmail.com>
2022-12-16 12:41:17 +0300

    refactor: add throwing Invalid input error on file rename

--- src/operations-with-file/rename.js
+++ src/operations-with-file/rename.js
@@ -3,33 +3,49 @@ import { join, dirname } from 'node:path';
 import { printCurrentDir } from '../messages/printCurrentDir.js';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
 import { checkIsFileExist } from '../utils/checkIsFileExist.js';
-import { cwd } from 'node:process';
 import { validateFilename } from '../utils/validateFilename.js';
 import { CustomError } from '../errors/CustomError.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
+import { isFile } from '../utils/isFile.js';
 
 export const rename = async ([pathToFile, newFilename]) => {
   try {
-    if (!newFilename) {
-      throw new CustomError('Please, provide a new file name');
+    if (!pathToFile) {
+      throw new InvalidInputError('Please, provide a path to the file name');
     }
 
-    const oldPath = join(cwd(), pathToFile);
-    const newPath = join(cwd(), dirname(pathToFile), newFilename);
+    if (!newFilename) {
+      throw new InvalidInputError('Please, provide a new file name');
+    }
 
-    if (await checkIsFileExist(newPath)) {
+    const pathToSourceFile = getAbsolutePath(pathToFile);
+    const pathToRenamedFile = join(dirname(pathToSourceFile), newFilename);
+    const isPathToFile = await isFile(pathToSourceFile);
+
+    if (!isPathToFile) {
+      throw new CustomError('Only files can be renamed');
+    }
+
+    if (await checkIsFileExist(pathToRenamedFile)) {
       throw new CustomError('A file with the same name already exists');
     }
 
     if (!validateFilename(newFilename)) {
       throw new CustomError(
-        '"/,|,\\" and white spaces are not allowed in a filename'
+        '"/|\\" and white spaces are not allowed in a filename'
       );
     }
 
-    await fs.rename(oldPath, newPath);
+    await fs.rename(pathToSourceFile, pathToRenamedFile);
 
     printCurrentDir();
   } catch (error) {
+    if (error instanceof InvalidInputError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
+    }
+
     if (error instanceof CustomError) console.log(error.message);
 
     throw new OperationFailedError('Operation failed');
