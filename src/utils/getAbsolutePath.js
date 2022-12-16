eaabbc3a3972a990965ad1e6086cb744123e12b9
Andrew <an.carev4@gmail.com>
2022-12-16 18:12:23 +0300

    feat: add handling passing the root directory as a path parameter

--- src/utils/getAbsolutePath.js
+++ src/utils/getAbsolutePath.js
@@ -1,7 +1,13 @@
 import { isAbsolute, join } from 'node:path';
 import { cwd } from 'node:process';
+import { homedir } from 'node:os';
+import { getRootDir } from './getRootDir.js';
 
 export const getAbsolutePath = (path) => {
+  const rootDir = getRootDir();
+
+  if (path === rootDir) return homedir();
+
   const isAbsolutePath = isAbsolute(path);
   const currentDir = cwd();
 
