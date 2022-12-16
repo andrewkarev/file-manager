d29ef1321cc6e66f21a3146cab8cab8f0b9e68d0
Andrew <an.carev4@gmail.com>
2022-12-16 13:56:55 +0300

    refactor: add throwing `Invalid input` error on file deletion

--- src/operations-with-file/delete.js
+++ src/operations-with-file/delete.js
@@ -1,17 +1,38 @@
 import { rm } from 'node:fs/promises';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
-import { join } from 'node:path';
-import { cwd } from 'node:process';
 import { printCurrentDir } from '../messages/printCurrentDir.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
+import { CustomError } from '../errors/CustomError.js';
+import { isFile } from '../utils/isFile.js';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
 
 export const remove = async ([path]) => {
   try {
-    const pathTofFile = join(cwd(), path);
+    if (!path) {
+      throw new InvalidInputError('Please, provide a path to the file');
+    }
+
+    const isPathToFile = await isFile(path);
+
+    if (!isPathToFile) {
+      throw new CustomError('Only files can be deleted');
+    }
+
+    const pathTofFile = getAbsolutePath(path);
 
     await rm(pathTofFile);
 
     printCurrentDir();
   } catch (error) {
+    if (error instanceof InvalidInputError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
+    }
+
+    if (error instanceof CustomError) {
+      console.log(error.message);
+    }
+
     throw new OperationFailedError('Operation failed');
   }
 };
