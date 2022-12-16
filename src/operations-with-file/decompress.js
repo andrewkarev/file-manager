cd6b506e987147ba3353f914765fd41bf4db41b4
Andrew <an.carev4@gmail.com>
2022-12-16 14:33:30 +0300

    refactor: add throwing `Invalid input` error on file decompressing

--- src/operations-with-file/decompress.js
+++ src/operations-with-file/decompress.js
@@ -1,30 +1,31 @@
-import { join, dirname, basename, extname } from 'node:path';
-import { cwd } from 'node:process';
+import { dirname, basename, join } from 'node:path';
 import { createReadStream, createWriteStream } from 'node:fs';
 import { createBrotliDecompress } from 'node:zlib';
 import { CustomError } from '../errors/CustomError.js';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
-import { checkFile } from '../utils/checkFile.js';
 import { checkIsFileExist } from '../utils/checkIsFileExist.js';
 import { printCurrentDir } from '../messages/printCurrentDir.js';
 import { validateFilename } from '../utils/validateFilename.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
+import { isFile } from '../utils/isFile.js';
 
 export const decompress = async ([pathToFile, pathToDestination]) => {
   try {
     if (!pathToFile) {
-      throw new CustomError('Please, provide a path to the source file');
+      throw new InvalidInputError('Please, provide a path to the source file');
     }
 
     if (!pathToDestination) {
-      throw new CustomError(
+      throw new InvalidInputError(
         'Please, provide the path to save the decompressed file'
       );
     }
 
-    const sourceFilePath = join(cwd(), pathToFile);
-    const decompressedDirPath = join(cwd(), dirname(pathToDestination));
+    const sourceFilePath = getAbsolutePath(pathToFile);
+    const decompressedFileDirPath = getAbsolutePath(dirname(pathToDestination));
     const isSourceFile = await checkIsFileExist(sourceFilePath);
-    const isDestinationDir = await checkIsFileExist(decompressedDirPath);
+    const isDestinationDir = await checkIsFileExist(decompressedFileDirPath);
 
     if (!isSourceFile || !isDestinationDir) {
       throw new CustomError(
@@ -32,27 +33,36 @@ export const decompress = async ([pathToFile, pathToDestination]) => {
       );
     }
 
-    const filename = basename(sourceFilePath);
-    const isFile = await checkFile(sourceFilePath, filename);
-
-    if (!isFile) {
+    const isPathToFile = await isFile(sourceFilePath);
+    if (!isPathToFile) {
       throw new CustomError('Only files can be decompressed');
     }
 
     const decompressedFileName = basename(pathToDestination);
 
-    if (await checkIsFileExist(decompressedFileName)) {
+    const decompressedFilePath = join(
+      decompressedFileDirPath,
+      decompressedFileName
+    );
+
+    if (await checkIsFileExist(decompressedFilePath)) {
       throw new CustomError('A file with the same name already exists');
     }
 
     if (!validateFilename(decompressedFileName)) {
       throw new CustomError(
-        '"/,|,\\" and white spaces are not allowed in a filename'
+        '"/|\\" and white spaces are not allowed in a filename'
+      );
+    }
+
+    if (!decompressedFileName.endsWith('.br')) {
+      throw new CustomError(
+        'You can only decompress files compressed with the Brotli algorithm that have the "br" extension'
       );
     }
 
     const readStream = createReadStream(sourceFilePath);
-    const writeStream = createWriteStream(pathToDestination);
+    const writeStream = createWriteStream(decompressedFilePath);
 
     const brotli = createBrotliDecompress();
 
@@ -64,6 +74,11 @@ export const decompress = async ([pathToFile, pathToDestination]) => {
     });
   } catch (error) {
     if (error instanceof CustomError) console.log(error.message);
+
+    if (error instanceof InvalidInputError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
+    }
 
     throw new OperationFailedError('Operation failed');
   }
