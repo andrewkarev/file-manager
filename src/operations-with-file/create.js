a401a8bdda20012f49f315034ac8f14d9ae53271
Andrew <an.carev4@gmail.com>
2022-12-16 12:26:51 +0300

    refactor: add throwing `Invalid input` error on file creation

--- src/operations-with-file/create.js
+++ src/operations-with-file/create.js
@@ -3,26 +3,36 @@ import { printCurrentDir } from '../messages/printCurrentDir.js';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
 import { validateFilename } from '../utils/validateFilename.js';
 import { CustomError } from '../errors/CustomError.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
 
 export const create = async ([filename]) => {
   try {
-    if (!filename) console.log('Please, provide a file name');
+    if (!filename) {
+      throw new InvalidInputError('Please, provide a file name');
+    }
 
-    if (!validateFilename(filename)) {
+    const isValidFilename = validateFilename(filename);
+
+    if (!isValidFilename) {
       throw new CustomError(
-        '"/,|,\\" and white spaces are not allowed in a filename'
+        '"/|\\" and white spaces are not allowed in a filename'
       );
     }
 
     const file = await open(filename, 'ax');
-    await file.close();
 
+    await file.close();
     printCurrentDir();
   } catch (error) {
     if (error instanceof CustomError) console.log(error.message);
 
     if (error.code === 'EEXIST') {
       console.log('A file with the same name already exists');
+    }
+
+    if (error instanceof InvalidInputError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
     }
 
     throw new OperationFailedError('Operation failed');
