1dd9dc093c6de657faf0bb014a94fa1aeada2f76
Andrew <an.carev4@gmail.com>
2022-12-16 10:36:22 +0300

    feat: implement handling passing absolute path as a parameter to the `goTo`

--- src/navigation/goTo.js
+++ src/navigation/goTo.js
@@ -1,12 +1,31 @@
 import { chdir } from 'node:process';
+import { homedir } from 'node:os';
+import { dirname } from 'node:path';
+import { CustomError } from '../errors/CustomError.js';
+import { InvalidInputError } from '../errors/InvalidInputError.js';
 import { OperationFailedError } from '../errors/OperationFailedError.js';
 import { printCurrentDir } from '../messages/printCurrentDir.js';
+import { getAbsolutePath } from '../utils/getAbsolutePath.js';
 
 export const goTo = ([path]) => {
   try {
-    chdir(path);
+    if (!path) {
+      throw new CustomError('Please, provide a path to change directory');
+    }
+
+    const homeDir = homedir();
+    const pathToGo = getAbsolutePath(path);
+    const isRootDir = pathToGo.toLowerCase() === dirname(homeDir).toLowerCase();
+
+    isRootDir ? chdir(homeDir) : chdir(pathToGo);
+
     printCurrentDir();
   } catch (error) {
+    if (error instanceof CustomError) {
+      console.log(error.message);
+      throw new InvalidInputError('Invalid input');
+    }
+
     throw new OperationFailedError('Operation failed');
   }
 };
